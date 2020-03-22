dde18849983234de4925ca54be55fe90733647fb
john0714 <john0714@naver.com>
2020-03-22 13:25:29 +0900

    ver 0.2

--- web_game_macro.py
+++ web_game_macro.py
@@ -21,116 +21,156 @@ while 1:
 		# Loading
 		# time.sleep(1)
 		
-		# Check Status
-		pane=driver.find_elements_by_id("pane_effects")[0]
-		
-		healthpot=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/healthpot.png')]")
-		regen=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/regen.png')]")
-		heartseeker=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/heartseeker.png')]")
-		shadowveil=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/shadowveil.png')]")
-		channeling=driver.find_elements_by_xpath("//img[contains(@src,'/y/e/channeling.png')]")
-		
-		spiritStatus=driver.find_elements_by_xpath("//img[contains(@src,'/y/battle/spirit_a.png')]")
-		
-		health=driver.find_elements_by_id("vrhd")[0]
-		mana=driver.find_elements_by_id("vrm")[0]
-		spirit=driver.find_elements_by_id("vrs")[0]
-		
-		goNextArena=driver.find_elements_by_id("btcp")
-		
+		# 포니문제
 		ponny=driver.find_elements_by_id("riddleanswer")
+			
+		# 다음 스테이지로 이동 체크
+		goNextArena=driver.find_elements_by_id("btcp")
 		
 		if ponny:
 			ponny.send_keys(str(random.randrange(1,3)))
 			ponny.submit()
-			continue
-		
-		if goNextArena:
+		elif goNextArena:
 			goNextArena[0].click()
 		else:
+			# 상태 변수 생성
+			pane=driver.find_elements_by_id("pane_effects")[0]
+			
+			healthpot=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/healthpot.png')]")
+			manapot=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/manapot.png')]")
+			spiritpot=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/spiritpot.png')]")
+			regen=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/regen.png')]")
+			heartseeker=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/heartseeker.png')]")
+			shadowveil=pane.find_elements_by_xpath("//img[contains(@src,'/y/e/shadowveil.png')]")
+			channeling=driver.find_elements_by_xpath("//img[contains(@src,'/y/e/channeling.png')]")
+			
+			spiritStatus=driver.find_elements_by_xpath("//img[contains(@src,'/y/battle/spirit_a.png')]")
+			
+			health=driver.find_elements_by_id("vrhd")
+			if health:
+				health=health[0]
+			else:
+				health=driver.find_elements_by_id("vrhb")[0]
+			
+			mana=driver.find_elements_by_id("vrm")[0]
+			spirit=driver.find_elements_by_id("vrs")[0]
+			
+			# 타겟 몬스터 설정
+			targetMonster=""
+			for n in range(1, len(driver.find_elements_by_xpath("//div[@id='pane_monster']/div"))+1):
+				monster=driver.find_elements_by_id("mkey_" + str(n))
+				if monster:
+					monster=monster[0]
+					if float(monster.value_of_css_property("opacity")) <= 0.3:
+						continue
+					elif monster.value_of_css_property("border-color") == "rgb(92, 13, 18)":
+						targetMonster=monster
+						break
+					elif monster.value_of_css_property("border-color") == "rgb(189, 116, 0)":
+						targetMonster=monster
+						break
+					else:
+						targetMonster=monster
+			
+			# 상태 변수 체크 및 행동
+			# 생존력을 높이기 위해 생존관련 체크를 앞에 두고 continue를 넣음
+			
+			# For spiritShield
+			if int(spirit.text) < 300:
+				item=driver.find_elements_by_id("ckey_items")[0]
+				item.click()
+				spiritPoint=driver.find_elements_by_id("ikey_6")
+				if spiritPoint:
+					spiritPoint[0].click()
+					continue
+				else:
+					item=driver.find_elements_by_id("ckey_items")[0]
+					item.click()
+			
+			# For Health
+			if int(health.text) < 12000:
+				fullCure=driver.find_elements_by_id("qb5")
+				if fullCure:
+					fullCure[0].click()
+					continue
+				else:
+					cure=driver.find_elements_by_id("qb1")
+					if cure:
+						cure[0].click()
+						continue
+					else:
+						item=driver.find_elements_by_id("ckey_items")[0]
+						item.click()
+						healthPotion=driver.find_elements_by_id("ikey_4")
+						if healthPotion:
+							healthPotion[0].click()
+							continue
+						else:
+							item=driver.find_elements_by_id("ckey_items")[0]
+							item.click()
+			
+			# For Mana
+			if int(mana.text) < 300:
+				item=driver.find_elements_by_id("ckey_items")[0]
+				item.click()
+				manaPotion=driver.find_elements_by_id("ikey_5")
+				if manaPotion:
+					manaPotion[0].click()
+					continue
+				else:
+					item=driver.find_elements_by_id("ckey_items")[0]
+					item.click()
+
 			if channeling:
 				spell=driver.find_elements_by_id("qb3")[0]
 				spell.click()
-				time.sleep(0.5)
+				continue
+			
+			if int(mana.text) < 700 and not manapot:
+				item=driver.find_elements_by_id("ckey_items")[0]
+				item.click()
+				manaDraught=driver.find_elements_by_id("ikey_2")[0]
+				manaDraught.click()
+				continue
+
+			if int(spirit.text) < 500 and not spiritpot:
+				item=driver.find_elements_by_id("ckey_items")[0]
+				item.click()
+				spiritDraught=driver.find_elements_by_id("ikey_3")[0]
+				spiritDraught.click()
+				continue
 		
 			if not healthpot:
 				item=driver.find_elements_by_id("ckey_items")[0]
 				item.click()
 				healthDraught=driver.find_elements_by_id("ikey_1")[0]
 				healthDraught.click()
-				time.sleep(0.5)
+				continue
 				
 			if not regen:
 				spell=driver.find_elements_by_id("qb2")[0]
 				spell.click()
-				time.sleep(0.5)
+				continue
 				
 			if not heartseeker:
 				spell=driver.find_elements_by_id("qb3")[0]
 				spell.click()
-				time.sleep(0.5)
+				continue
 
 			if not shadowveil:
 				spell=driver.find_elements_by_id("qb4")[0]
 				spell.click()
-				time.sleep(0.5)
-				
+				continue
+			
+			# 스피릿 상태
 			if not spiritStatus:
 				energy=driver.find_elements_by_xpath("//div[@id='vcp']/div/div")
 				if len(energy) >= 10:
 					driver.find_elements_by_id("ckey_spirit")[0].click()
-					time.sleep(0.5)
-					
-			if int(health.text) < 10000:
-				fullCure=driver.find_elements_by_id("qb5")
-				if fullCure:
-					fullCure[0].click()
-					time.sleep(0.5)
-				else:
-					cure=driver.find_elements_by_id("qb1")
-					if cure:
-						cure[0].click()
-						time.sleep(0.5)
-					else:
-						item=driver.find_elements_by_id("ckey_items")[0]
-						item.click()
-						healthPotion=driver.find_elements_by_id("ikey_4")[0]
-						healthPotion.click()
-						time.sleep(0.5)
-						
-			if int(mana.text) < 700:
-				item=driver.find_elements_by_id("ckey_items")[0]
-				item.click()
-				manaDraught=driver.find_elements_by_id("ikey_2")
-				if manaDraught:
-					manaDraught[0].click()
-					time.sleep(0.5)
-				else:
-					item=driver.find_elements_by_id("ckey_items")[0]
-					item.click()
-						
-			if int(mana.text) < 300:
-				item=driver.find_elements_by_id("ckey_items")[0]
-				item.click()
-				manaPotion=driver.find_elements_by_id("ikey_5")[0]
-				manaPotion.click()
-				time.sleep(0.5)
-				
-			if int(spirit.text) < 500:
-				item=driver.find_elements_by_id("ckey_items")[0]
-				item.click()
-				spiritDraught=driver.find_elements_by_id("ikey_3")
-				if spiritDraught:
-					spiritDraught[0].click()
-					time.sleep(0.5)
-				else:
-					item=driver.find_elements_by_id("ckey_items")[0]
-					item.click()
-				
-			for n in range(1, 10):
-				monster=driver.find_elements_by_id("mkey_" + str(n))[0]
-				if monster and float(monster.value_of_css_property("opacity")) > 0.3:
-					monster.click()
-					break
-	except:
+					continue
+			
+			# 공격
+			targetMonster.click()
+			
+	except Exception:
 		pass
